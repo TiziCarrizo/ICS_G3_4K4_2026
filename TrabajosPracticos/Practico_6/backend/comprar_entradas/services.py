d5b65b316f56405cb15226e428bea62b6969037f
FelippaAlexis <90843@sistemas.frc.utn.edu.ar>
2026-05-30 14:49:48 -0300

    90843: feat pruebas de validacion iniciales fallando (RED)

---
+++ TrabajosPracticos/Practico_6/backend/comprar_entradas/services.py
@@ -0,0 +1,11 @@
+def validar_usuario_registrado(usuario):
+    pass
+
+def validar_cantidad_entradas(cantidad):
+    pass
+
+def validar_fecha_visita(fecha, feriados=None):
+    pass
+
+def validar_forma_pago(forma_pago):
+    pass
---
+++ TrabajosPracticos/Practico_6/backend/tests/unit/test_realizar_compra.py
@@ -0,0 +1,66 @@
+import pytest
+from datetime import date, timedelta
+
+try:
+    from comprar_entradas.services import (
+        validar_usuario_registrado,
+        validar_cantidad_entradas,
+        validar_fecha_visita,
+        validar_forma_pago
+    )
+except ImportError:
+    pytest.fail("Faltan definir las funciones de validación en comprar_entradas/services.py")
+
+
+@pytest.fixture
+def fecha_futura():
+    # Retorna una fecha 5 días en el futuro
+    return date.today() + timedelta(days=5)
+
+
+# --- TESTS RED ---
+
+def test_validar_usuario_rechaza_usuario_no_registrado():
+   
+    usuario_invalido = {"nombre": "Visitante Anónimo"} # Falta el ID
+    
+  
+    with pytest.raises(ValueError, match="El usuario no está registrado"):
+        validar_usuario_registrado(usuario_invalido)
+
+
+def test_validar_cantidad_entradas_rechaza_mas_de_10():
+   
+    cantidad_invalida = 11
+    
+   
+    with pytest.raises(ValueError, match="La cantidad de entradas supera el máximo permitido"):
+        validar_cantidad_entradas(cantidad_invalida)
+
+
+def test_validar_fecha_rechaza_fechas_pasadas():
+    
+    fecha_pasada = date.today() - timedelta(days=1)
+    
+   
+    with pytest.raises(ValueError, match="La fecha de visita no puede ser una fecha pasada"):
+        validar_fecha_visita(fecha_pasada)
+
+
+def test_validar_fecha_rechaza_dias_cerrados(fecha_futura):
+    
+    dias_para_lunes = (0 - fecha_futura.weekday()) % 7
+    fecha_lunes = fecha_futura + timedelta(days=dias_para_lunes)
+    
+   
+    with pytest.raises(ValueError, match="El parque está cerrado los lunes"):
+        validar_fecha_visita(fecha_lunes)
+
+
+def test_validar_forma_pago_rechaza_opcion_invalida():
+    
+    pago_invalido = "TRANSFERENCIA"
+    
+   
+    with pytest.raises(ValueError, match="Forma de pago no válida"):
+        validar_forma_pago(pago_invalido)
