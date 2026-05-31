2f897fa939747d80d29aa4b0b34626786d452223
MauroGenesir <90090@sistemas.frc.utn.edu.ar>
2026-05-31 13:43:08 -0300

    90090: Página principal casi terminada

--- TrabajosPracticos/Practico_6/frontend/src/app/app.ts
+++ TrabajosPracticos/Practico_6/frontend/src/app/app.ts
@@ -1,6 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface AppUser {
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+}
+
+export const MOCK_USER: AppUser = {
+    nombre: "Manuel",
+    apellido: "Dávila",
+    email: "manueldavila@example.com",
+    id: 1
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
---
+++ TrabajosPracticos/Practico_6/frontend/src/app/pages/comprar-entrada/comprar-entrada.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comprar-entrada',
+  imports: [],
+  templateUrl: './comprar-entrada.html',
+  styleUrl: './comprar-entrada.scss',
+})
+export class ComprarEntrada {}
