21e800e7d324989eea5501e701588dbf71b0cc32
FelippaAlexis <90843@sistemas.frc.utn.edu.ar>
2026-05-31 21:46:45 -0300

    90843: feat: crear endpoint de compras e integracion basica (GREEN)

--- TrabajosPracticos/Practico_6/backend/comprar_entradas/views.py
+++ TrabajosPracticos/Practico_6/backend/comprar_entradas/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .services import procesar_compra
 
-# Create your views here.
+@csrf_exempt
+def api_realizar_compra(request):
+    if request.method == 'POST':
+        try:
+            datos = json.loads(request.body)
+            
+            if "fecha" in datos and isinstance(datos["fecha"], str):
+                datos["fecha"] = datetime.strptime(datos["fecha"], "%Y-%m-%d").date()
+            
+            compra = procesar_compra(datos)
+            
+            return JsonResponse({"mensaje": "Compra procesada exitosamente"}, status=201)
+            
+        except ValueError as e:
+            # Atrapamos nuestros errores de validación de negocio y los devolvemos como 400 Bad Request
+            return JsonResponse({"error": str(e)}, status=400)
+            
+    return JsonResponse({"error": "Método no permitido"}, status=405)
--- TrabajosPracticos/Practico_6/backend/tests/integration/test_api_compras.py
+++ TrabajosPracticos/Practico_6/backend/tests/integration/test_api_compras.py
@@ -12,14 +12,13 @@ def test_api_realizar_compra_endpoint_retorna_201(client):
     # Este es el JSON exacto que te va a mandar tu código de frontend
     payload = {
         "usuario": {"id": usuario_real.id, "nombre": usuario_real.nombre},
-        "fecha": "2026-06-15",
+        "fecha": "2026-06-16",
         "forma_pago": "TARJETA",
         "entradas": [
             {"edad": 25, "tipo_pase": "VIP", "precio_unitario": 5000.0}
         ]
     }
     
-    # Act: Simulamos un POST request desde el cliente (React/HTML) hacia nuestra futura ruta
     response = client.post(
         '/api/compras/', 
         data=json.dumps(payload),
